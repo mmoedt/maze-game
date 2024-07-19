9de99284c8289edc4280efc21f0cb3fe58b65451
Michael Moedt <michael.moedt@treez.io>
2024-07-19 10:22:56 -0700

    Updating notes and after running npx create-react-app (part 1)

--- go.sh
+++ go.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+echo "Setting up environment..."
+source ./my-env.sh
+nvm install  # just in case; the env script doesn't install missing versions
+
+echo "Building the project..."
+npm run build
+
+echo "Launching the project in a chrome browser window..."
 google-chrome --profile-directory="Profile Maze-Game" --new-window "${PWD}/dist/index.html"
 
