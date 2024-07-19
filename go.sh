58eeeafdfc639078c04800275441154d1c977da3
Michael Moedt <michael.moedt@treez.io>
2024-07-19 11:00:21 -0700

    quick script update re: new layout

--- go.sh
+++ go.sh
@@ -4,8 +4,12 @@ echo "Setting up environment..."
 source ./my-env.sh
 nvm install  # just in case; the env script doesn't install missing versions
 
-echo "Building the project..."
-npm run build
+(
+    cd game
+    [ -e env.sh ] && source ./env.sh
+    echo "Building the project..."
+    npm run build
+)
 
 echo "Launching the project in a chrome browser window..."
 google-chrome --profile-directory="Profile Maze-Game" --new-window "${PWD}/dist/index.html"
